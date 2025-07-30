afd16ebb9b7b4768a11906df9d02202a68b92e75
pengwangfd <pengwang5.fd@gmail.com>
2025-07-30 18:10:36 +0800

    Add GitHub Pages deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -24,5 +24,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'), // ✅ 定义 @ = src
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/LabVue/' : '/',
+  base: '/',
 })
